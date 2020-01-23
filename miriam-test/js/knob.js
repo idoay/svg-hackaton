d25fa4fead28c3de742d25152394afbb76261557
Miriam Zusin <1rosehip@gmail.com>
2020-01-23 16:19:23 +0200

    test

--- miriam-test/js/knob.js
+++ miriam-test/js/knob.js
@@ -16,15 +16,15 @@
 
         $(document).on('mousemove', function(evt){
 
-            var angle = Math.round(Math.atan2(evt.pageX - boxCenter[0], -(evt.pageY - boxCenter[1]) )*(180/Math.PI) + 180);
+            var angle = Math.round(Math.atan2(evt.pageX - boxCenter[0], -(evt.pageY - boxCenter[1]) )*(180/Math.PI));
 
-            if(angle >= fromAngle && angle <= toAngle){
+            //if(angle >= fromAngle && angle <= toAngle){
 
                 var formattedAngle = 'rotate(' + angle + 'deg)';
                 $handle.css({ 'transform': formattedAngle});
                 $handle.css({ "-webkit-transform": formattedAngle });
                 $handle.css({ '-moz-transform': formattedAngle });
-            }
+            //}
         });
 
         $(document).on('mouseup', function(){
